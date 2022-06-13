e2585dd0c12b9fb58ed99a8b43fcd693b89930ca
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-13 13:42:06 -0400

    Create Classroom.java

---
+++ src/Classroom.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class Classroom {
+	
+	// data members
+	private String name;
+	private int num;
+	private ArrayList<Student> peers;
+	private Teacher teacher;
+	
+	// constructors
+	public Classroom(String name, int num, ArrayList<Student> peers, Teacher teacher) {
+		this.name = name;
+		this.num = num;
+		this.peers = peers;
+		this.teacher = teacher;
+	}
+	
+	// methods
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public ArrayList<Student> getPeers() {
+		return peers;
+	}
+	public void setPeers(ArrayList<Student> peers) {
+		this.peers = peers;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+}
