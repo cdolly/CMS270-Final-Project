ba067663b4a75ecc6e82523da7bde27a231031f6
cdolly <72312670+cdolly@users.noreply.github.com>
2022-06-13 13:44:09 -0400

    Create Stranger.java

---
+++ src/Stranger.java
@@ -0,0 +1,11 @@
+public class Stranger extends Person{
+	
+
+	// constructors
+	public Stranger(boolean isInfected, boolean isQuarantined, boolean isImmune, int daysInQuarantine,
+			float infectionProb, String name, int age) {
+		super(isInfected, isQuarantined, isImmune, daysInQuarantine, infectionProb, name, age);
+	}
+	
+	
+}
